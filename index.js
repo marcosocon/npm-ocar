edd13662b485887a3c69d4317a290311d5b270d5
marcosocon <oconmarcos97@gmail.com>
2016-03-30 20:25:26 -0300

    improved functionality

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
 var request = require("request");
-exports.ocarequest = function(number){
+module.exports = function (number){
 
   var TYPES = ["cartas", "paquetes", "dni", "partidas"];
   var q = "package-locator";
   var results = [];
+  var array = [];
   TYPES.map(function(type){
   request.get({
     url:'https://www.oca.com.ar/?q='+q+'&type='+type+'&number='+number
   },
   function(err,response,body){
-    if (response.body.indexOf('"success":false') != -1) {
-      console.log("no se encontraron " + type);
-    }
-    else{
+    if (response.body.indexOf('"success":true') != -1) {
       var res = JSON.parse(body)
-      results.push(res.data)
+      array.push(res.data)
+      var results = array[0][0];
       console.log(results);
     }
+    else{
+      console.log("No se encontraron " + type);
+    }
+    });
   });
-})
-
-}
+};
