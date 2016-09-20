840315acb905929733fe65dc21a56b818690fd85
marcosocon <oconmarcos97@gmail.com>
2016-09-19 23:48:45 -0300

    refactor and added request per type

--- index.js
+++ index.js
@@ -1,24 +1,36 @@
 var request = require("request");
-module.exports = function (number){
-
-  var TYPES = ["cartas", "paquetes", "dni", "partidas"];
-  var q = "package-locator";
-  var results = [];
-  var array = [];
-  TYPES.map(function(type){
-  request.get({
-    url:'https://www.oca.com.ar/?q='+q+'&type='+type+'&number='+number
-  },
-  function(err,response,body){
-    if (response.body.indexOf('"success":true') != -1) {
-      var res = JSON.parse(body)
-      array.push(res.data)
-      var results = array[0][0];
-      console.log(results);
-    }
-    else{
-      console.log("No se encontraron " + type);
-    }
-    });
-  });
+module.exports = function(number, type){
+	if (!number || !type) {
+		console.log("Por favor ingresa ambos valores, track id y tipo");
+		return false;
+	}
+	if (type !== 'dni' && type !== 'cartas' && type !== 'partidas' && type !== 'paquetes') {
+		console.log("Tipo de envio invalido");
+		return false;
+	}
+	request.get({
+		url:'https://www.oca.com.ar/?q=package-locator&type='+type+'&number='+number
+	},
+	function(req, response, err){
+		if (response.body) {
+			var body = JSON.parse(response.body);
+			if (body.success) {
+				var array = body.data[0];
+				var result = {
+					"Tipo": array.tipo,
+					"Numero de pieza" : array.numeroPieza,
+					"Nombre del titular" : array.titular,
+					"Descripcion": array.descripcion,
+					"Fecha": array.fecha,
+					"Sucursal": array.sucursal
+				};
+				console.log(result);
+				return result;
+			} else {
+				console.log("No se encontraron", type);
+			}
+		} else {
+			console.log("Ha ocurrido un error");
+		}
+	});
 };
